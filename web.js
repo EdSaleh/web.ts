a4789ef864a00663a7484fa23f4b69131f2dbfcd
medozs <ahmedzs@outlook.com>
2015-12-24 01:57:34 -0500

    new version

--- web.js
+++ web.js
@@ -1,146 +1,7 @@
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
-//Web.ts
 var web;
 (function (web) {
     var ts;
     (function (ts) {
-        function main(e) {
-            /*
-                code
-             */
-            //code for pages
-            switch (window.location.href.substr(window.location.href.lastIndexOf("#") + 1)) {
-                case "":
-                case "Index":
-                default:
-                    var Index = (function (_super) {
-                        __extends(Index, _super);
-                        function Index() {
-                            _super.apply(this, arguments);
-                        }
-                        //view page for action
-                        Index.prototype.view = function () {
-                            return "/Index.txt";
-                        };
-                        //how to render document method
-                        Index.prototype.render = function (doc) {
-                            document.title = "Index";
-                            document.getElementById("content").innerHTML = doc.body.innerHTML;
-                        };
-                        ;
-                        return Index;
-                    })(web.ts.Page);
-                    (new Index());
-                    break;
-                case "element":
-                    //get Template from an element on Page(web ts css class makes the element hidden)
-                    var example = (function (_super) {
-                        __extends(example, _super);
-                        function example() {
-                            _super.apply(this, arguments);
-                        }
-                        //public Renderer: Function;
-                        example.prototype.view = function () {
-                            return "#element"; //(get element with id #)
-                        };
-                        example.prototype.render = function (doc) {
-                            document.title = "Element";
-                            document.getElementById("content").innerHTML = doc.body.innerHTML;
-                        };
-                        ;
-                        return example;
-                    })(web.ts.Page);
-                    (new example());
-                    break;
-                case "list":
-                    var List_1 = (function (_super) {
-                        __extends(List_1, _super);
-                        function List_1() {
-                            _super.apply(this, arguments);
-                        }
-                        List_1.prototype.view = function () {
-                            return "#element"; // just use the current document as the view. 
-                        };
-                        List_1.prototype.add = function (item, i, doc) {
-                            var _this = this;
-                            if (doc === void 0) { doc = this.doc; }
-                            var elm = docElement(doc);
-                            elm.innerText = item;
-                            elm.onclick = function () { return _this.remove(item); };
-                            document.getElementById("content").appendChild(elm);
-                        };
-                        List_1.prototype.remove = function (item, i) {
-                            if (i === void 0) { i = null; }
-                            if (item != null) {
-                                var elms = document.getElementById("content").children;
-                                for (var index = 0; index < elms.length; index++)
-                                    if (elms[index].innerText == item)
-                                        elms[index].remove();
-                            }
-                            else if (i != null) {
-                                document.getElementById("content").children[i].remove();
-                            }
-                        };
-                        List_1.prototype.set = function (fill, doc) {
-                            if (fill === void 0) { fill = true; }
-                            document.getElementById("content").innerHTML = "";
-                            if (fill) {
-                                var strs = ["click on any item to remove", "a", "b", "c"];
-                                this.list(strs);
-                                this.add("d");
-                                this.remove(null, 2);
-                                this.list(["e", "f", "g"]);
-                            }
-                        };
-                        return List_1;
-                    })(web.ts.List);
-                    var list = new List_1(); //Do the operation
-                    break;
-                case "view":
-                    //View Example
-                    var View_1 = (function (_super) {
-                        __extends(View_1, _super);
-                        function View_1() {
-                            _super.apply(this, arguments);
-                        }
-                        View_1.prototype.apply = function (text, doc) {
-                            if (doc === void 0) { doc = this.doc; }
-                            var elm = doc.body.children[0];
-                            document.title = "View Element";
-                            elm.textContent = "a document fetched using View() class. apply() is used to change and apply new items to the template elemnet";
-                            document.getElementById("content").appendChild(elm);
-                        };
-                        View_1.prototype.view = function () { document.getElementById("content").innerText = ""; return "#element"; };
-                        return View_1;
-                    })(web.ts.View);
-                    var view = new View_1();
-                    break;
-            }
-            /*
-                code
-            */
-        }
-        //Assign main() to Events
-        window.onload = function () {
-            main(null);
-            if ("onhashchange" in window) {
-                onhashchange = main;
-            }
-            else {
-                //If not supported
-                var elms = document.getElementsByTagName("a");
-                for (var i = 0; i < elms.length; i++)
-                    if (elms[i].classList.contains("web")) {
-                        elms[i].onclick = function () {
-                            window.location.reload();
-                        };
-                    }
-            }
-        };
         /*** web.ts libraries ***/
         //Hide Templates
         var style = document.createElement('style');
@@ -156,75 +17,44 @@ var web;
             Page.prototype.load = function () {
                 var _this = this;
                 var view = this.view();
-                if (view != null && view.length > 1) {
-                    if (view[0] != "#") {
-                        var xhttp = new XMLHttpRequest();
-                        xhttp.onload = function () { return _this.render(TextToDocument(xhttp.responseText)); };
-                        xhttp.open(view.lastIndexOf("?") < 0 && view.lastIndexOf(".") > view.lastIndexOf("/") ? "GET" : "POST", view, true);
-                        xhttp.send();
-                    }
-                    else {
-                        this.render(TextToDocument(document.getElementById(view.substr(1)).outerHTML));
-                    }
+                if (view != null && view.length > 1 && view[0] != "#") {
+                    var xhttp = new XMLHttpRequest();
+                    xhttp.onload = function () { return _this.render(TextToDocument(xhttp.responseText)); };
+                    xhttp.open(view.lastIndexOf("?") < 0 && view.lastIndexOf(".") > view.lastIndexOf("/") ? "GET" : "POST", view, true);
+                    xhttp.send();
                 }
             };
             return Page;
         })();
         ts.Page = Page;
         function TextToDocument(text) {
-            return (new DOMParser().parseFromString(text.replace(/(class( *)=["'][ \-\w]*web ts[ \-\w]*["'])?/gi, text.match(/(class( *)=["'][ \-\w]*web ts[ \-\w]*["'])?/gi)[0].replace(" ts", "")).replace(/(id( *)=["'][ \-\w]*["'])?/gi, ""), "text/html"));
+            return (new DOMParser().parseFromString(text, "text/html"));
         }
-        var View = (function (_super) {
-            __extends(View, _super);
-            function View() {
-                _super.call(this);
-                this.doc = document;
-            }
-            View.prototype.view = function () { return null; };
-            //Application method to apply work on the view
-            View.prototype.apply = function (item, doc) {
-                if (doc === void 0) { doc = this.doc; }
-            };
-            View.prototype.render = function (doc) {
-                this.doc = doc;
-                this.apply(null);
-            };
-            return View;
-        })(Page);
-        ts.View = View;
-        var List = (function (_super) {
-            __extends(List, _super);
+        var List = (function () {
             function List() {
-                _super.call(this);
-                this.doc = document;
             }
-            //Add and Remove Items Template
-            List.prototype.add = function (item, i, doc) {
-                if (i === void 0) { i = null; }
-                if (doc === void 0) { doc = this.doc; }
-            };
-            List.prototype.remove = function (item, i, doc) {
-                if (i === void 0) { i = null; }
-                if (doc === void 0) { doc = this.doc; }
-            };
-            List.prototype.set = function (fill, doc) {
-                if (fill === void 0) { fill = true; }
-                if (doc === void 0) { doc = this.doc; }
-            };
-            List.prototype.length = function () { };
-            List.prototype.render = function (doc) {
-                this.doc = doc;
-                this.set(true);
-            };
             //Start List Item Function
-            List.prototype.list = function (items) {
-                for (var i = 0; i < items.length; i++) {
-                    this.add(items[i]);
+            List.prototype.addRange = function (items, i) {
+                if (i === void 0) { i = this.length(); }
+                for (var indx = 0; indx < items.length; indx++) {
+                    this.add(items[indx], i + indx);
                 }
             };
             return List;
-        })(Page);
+        })();
         ts.List = List;
+        function getElement(edoc) {
+            if (edoc.nodeName.toLowerCase() === "#document") {
+                return (edoc.body.firstChild.cloneNode(true));
+            }
+            else {
+                var elm = (edoc.cloneNode(true));
+                elm.classList.remove("ts");
+                elm.id = "";
+                return elm;
+            }
+        }
+        ts.getElement = getElement;
         //ajax get resource
         function get(url, callback, timeout, timeoutcallback, type, async) {
             if (timeout === void 0) { timeout = 4000; }
@@ -239,10 +69,6 @@ var web;
             xhttp.send();
         }
         ts.get = get;
-        function docElement(doc) {
-            return (doc.body.firstChild.cloneNode(true));
-        }
-        ts.docElement = docElement;
     })(ts = web.ts || (web.ts = {}));
 })(web || (web = {}));
 //# sourceMappingURL=web.js.map
--- web.ts
+++ web.ts
@@ -1,115 +1,11 @@
-﻿//Web.ts
-module web.ts {
-    function main(e): void {
-        /* 
-            code
-         */
-        //code for pages
-        switch (window.location.href.substr(window.location.href.lastIndexOf("#") + 1)) {
-            case "": case "Index": default:
-                class Index extends web.ts.Page {
-                    //view page for action
-                    protected view(): string {
-                        return "/Index.txt";
-                    }
-                    //how to render document method
-                    protected render(doc: Document) {
-                        document.title = "Index";
-                        document.getElementById("content").innerHTML = doc.body.innerHTML;
-                    };
-                } (new Index());
-                break;
-            case "element":
-                //get Template from an element on Page(web ts css class makes the element hidden)
-                class example extends web.ts.Page {
-                    //public Renderer: Function;
-                    protected view(): string {
-                        return "#element";//(get element with id #)
-                    }
-                    protected render(doc: Document) {
-                        document.title = "Element";
-                        document.getElementById("content").innerHTML = doc.body.innerHTML;
-                    };
-                } (new example());
-                break;
-            case "list":
-                class List extends web.ts.List<string>{
-                    protected view() {
-                        return "#element";// just use the current document as the view. 
-                    }
-                    public add(item: string, i?: number, doc: Document = this.doc) {
-                        var elm = docElement(doc); 
-                        elm.innerText = item;
-                        elm.onclick = () => this.remove(item);
-                        document.getElementById("content").appendChild(elm);
-                    }
-                    public remove(item: string, i: number = null) {
-                        if (item != null) {
-                            var elms = document.getElementById("content").children;
-                            for (var index = 0; index < elms.length; index++)
-                                if ((<HTMLDivElement>elms[index]).innerText == item) (<HTMLDivElement>elms[index]).remove();
-                        } else if (i != null) {
-                            document.getElementById("content").children[i].remove();
-                        }
-                    }
-                    public set(fill: boolean = true, doc?: Document) {
-                        document.getElementById("content").innerHTML = "";
-                        if (fill) {
-                            var strs = ["click on any item to remove", "a", "b", "c"];
-                            this.list(strs);
-                            this.add("d");
-                            this.remove(null, 2);
-                            this.list(<string[]>["e", "f", "g"])
-                        }
-                    }
-                }
-                var list = new List(); //Do the operation
-                break;
-            case "view":
-                //View Example
-                class View extends web.ts.View<string>{
-                    public apply(text: string, doc: Document = this.doc) {
-                        var elm = doc.body.children[0];
-                        document.title = "View Element";
-                        elm.textContent = "a document fetched using View() class. apply() is used to change and apply new items to the template elemnet";
-                        document.getElementById("content").appendChild(elm);
-                    }
-                    protected view(): string { document.getElementById("content").innerText = ""; return "#element" }
-                }
-                var view = new View();
-                break;
-            //************End Change*******************
-        }
-        /*
-            code
-        */
-    }
-    //Assign main() to Events
-    window.onload = function () {
-        main(null);
-        if ("onhashchange" in window) {
-            onhashchange = main;
-        } else {
-        //If not supported
-            var elms = document.getElementsByTagName("a");
-            for (var i = 0; i < elms.length; i++)
-                if ((<HTMLAnchorElement>elms[i]).classList.contains("web")) {
-                    (<HTMLAnchorElement>elms[i]).onclick = function () {
-                        window.location.reload();
-                    }
-            }
-        }
-    }
-
-
-
-
+﻿module web.ts {
     /*** web.ts libraries ***/
     //Hide Templates
     var style = document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = '.web.ts { display: none; }';
     document.getElementsByTagName('head')[0].appendChild(style);
+
     /*** Library ***/
     export abstract class Page {
         constructor() {
@@ -120,57 +16,48 @@ module web.ts {
         //Loading Function
         private load() {
             var view: string = this.view();
-            if (view != null && view.length > 1) {
-                if (view[0] != "#") {
-                    var xhttp = new XMLHttpRequest();
-                    xhttp.onload = () => this.render(TextToDocument(xhttp.responseText));
-                    xhttp.open(view.lastIndexOf("?")<0 && view.lastIndexOf(".") > view.lastIndexOf("/")?"GET":"POST", view, true);
-                    xhttp.send();
-                } else {
-                    this.render(TextToDocument(document.getElementById(view.substr(1)).outerHTML));
-                }
+            if (view != null && view.length > 1 && view[0] != "#") {
+                var xhttp = new XMLHttpRequest();
+                xhttp.onload = () => this.render(TextToDocument(xhttp.responseText));
+                xhttp.open(view.lastIndexOf("?")<0 && view.lastIndexOf(".") > view.lastIndexOf("/")?"GET":"POST", view, true);
+                xhttp.send();
             }
         }
     }
     function TextToDocument(text: string): Document {
-        return <Document>(new DOMParser().parseFromString(text.replace(/(class( *)=["'][ \-\w]*web ts[ \-\w]*["'])?/gi, text.match(/(class( *)=["'][ \-\w]*web ts[ \-\w]*["'])?/gi)[0].replace(" ts", "")).replace(/(id( *)=["'][ \-\w]*["'])?/gi, ""), "text/html"))
-    }
-    export abstract class View<T> extends Page {
-        constructor() {
-            super();
-        }
-        protected doc: Document = document;
-        protected view():string {return null;}
-        //Application method to apply work on the view
-        public apply(item: T, doc: Document = this.doc): void { }
-        protected render(doc: Document) {
-            this.doc = doc;
-            this.apply(null);
-        }
+        return <Document>(new DOMParser().parseFromString(text, "text/html"));
     }
 
-    export abstract class List<T> extends Page {
-        constructor() {
-            super();
-        }
-        protected doc: Document = document;
-        //Add and Remove Items Template
-        public add(item: T, i: number = null, doc: Document = this.doc): void { }
-        public remove(item: T, i: number = null, doc: Document = this.doc): void { }
-        public set(fill: boolean = true, doc: Document = this.doc): void { }
-        public length(): void { }
-        protected render(doc: Document) {
-            this.doc = doc;
-            this.set(true);
-        }
+    export interface  View<T> {
+        //Application method to apply work on the view
+        apply(item: T);
+    }
+
+    export abstract class List< T > {
+        abstract add(item: T, i: number): void;
+        abstract remove(item: T, i: number): void;
+        abstract reset(): void;
+        abstract length(): number;
         //Start List Item Function
-        public list(items: T[]): void {
-            for (var i = 0; i < items.length;i++) {
-                this.add(<T>items[i]);
+        public addRange(items: T[], i: number = this.length()): void {
+            for (var indx = 0; indx < items.length;indx++) {
+                this.add(<T>items[indx],i+indx);
             }
         }
     }
 
+
+    export function getElement(edoc: Document | HTMLElement): HTMLElement {
+        if (edoc.nodeName.toLowerCase() === "#document") {
+            return (<HTMLElement>((<Document>edoc).body.firstChild.cloneNode(true)));
+        }
+        else{
+            var elm: HTMLElement = <HTMLElement>((<HTMLElement>edoc).cloneNode(true));
+            elm.classList.remove("ts");
+            elm.id = "";
+            return elm;
+        }
+    }
     //ajax get resource
     export function get(url: string, callback: Function, timeout: number = 4000, timeoutcallback: Function = () => { }, type: string = "GET", async: boolean = true): void {
         var xhttp = new XMLHttpRequest();
@@ -179,8 +66,5 @@ module web.ts {
         xhttp.ontimeout = timeoutcallback();
         xhttp.open(type, url, async);
         xhttp.send();
-    }
-    export function docElement(doc: Document) {
-        return (<HTMLElement>(doc.body.firstChild.cloneNode(true)));
     }
 }
